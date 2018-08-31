d59c0124ccdad8e84942a88eaa9dbdb3a9a10f50
dkersbergen <d.kersbergen@outlook.com>
2018-08-31 11:05:33 +0200

    Small edit on view data

--- scripts/map.js
+++ scripts/map.js
@@ -888,8 +888,9 @@ $(window).on('load', function() {
    * Changes map attribution (author, GitHub repo, email etc.) in bottom-right
    */
   function changeAttribution() {
-    /**
+    
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
+    /**
     var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
     var name = getSetting('_authorName');
     var url = getSetting('_authorURL');
