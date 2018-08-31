7e50ebccdf6a32d319c82d71821f9312d76d4b10
dkersbergen <d.kersbergen@outlook.com>
2018-08-31 10:59:59 +0200

    Remove view data
    Removed the link to view data for security reasons

--- scripts/map.js
+++ scripts/map.js
@@ -888,6 +888,7 @@ $(window).on('load', function() {
    * Changes map attribution (author, GitHub repo, email etc.) in bottom-right
    */
   function changeAttribution() {
+    /**
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
     var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
     var name = getSetting('_authorName');
@@ -901,7 +902,7 @@ $(window).on('load', function() {
     } else {
       credit += ' | ';
     }
-
+    */
     credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
     if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
     credit += ' with ';
